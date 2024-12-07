3b12b1f16b8348b86c75d11e639d9c538f50bb93
Sneha Mourya <157492674+Saini32@users.noreply.github.com>
2024-12-07 19:33:33 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class add {
+    public static void main(String args[])
+    {
+        int a,b,sum;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter 2 numbers=");
+        a=sc.nextInt();
+        b=sc.nextInt();
+        sum=a+b;
+        System.out.println("sum="+sum);
+
+    }
+    
+}
