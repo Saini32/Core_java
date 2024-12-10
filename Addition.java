0af423c368eca905dfa5b4b84d31679fd79c2121
Sneha Mourya <157492674+Saini32@users.noreply.github.com>
2024-12-10 22:30:57 +0530

    Command line argument concept

---
+++ Addition.java
@@ -0,0 +1,11 @@
+
+class Addition{
+    public static void main(String args [])
+    {
+        int a,b;
+        a = Integer.parseInt (args[0]);
+        b = Integer.parseInt (args[1]);
+        int res= a+b;
+        System.out.println("addition="+res);
+    }
+}
