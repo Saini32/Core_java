c250db77087e1362efbe7cfd16170abc1088fe8e
Sneha Mourya <157492674+Saini32@users.noreply.github.com>
2024-12-11 19:51:35 +0530

    weeks

---
+++ nested_ifelse.java
@@ -0,0 +1,46 @@
+//import java.util.Scanner;
+
+public class nested_ifelse {
+    public static void main(String[] args) {
+        
+        int day;
+        //Scanner sc=new Scannner(System.in);
+        day=Integer.parseInt(args[0]);
+      // System.out.println("eneter days of number=");
+      // day=sc.nextInt();
+        if (day==1) 
+        {
+            System.out.println("monday");
+            
+        } else if(day==2)
+        {
+            System.out.println("tuesday");
+        } 
+        else if(day==3)
+        {
+            System.out.println("wednesday");
+        }
+        else if(day==4)
+        {
+            System.out.println("thursday");
+        }
+        else if (day==5)
+        {
+            System.out.println("friday");
+        }
+        else if(day==6)
+        {
+            System.out.println("saturday");
+        }
+        else if (day==7)
+        {
+            System.out.println("sunday");
+        }
+        else
+        {
+            System.out.println("invalid!!");
+        }
+
+    }
+    
+}
