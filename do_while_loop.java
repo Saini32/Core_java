f163699fc7ca985dc4d1faa0f7f352b4edd453d8
Sneha Mourya <157492674+Saini32@users.noreply.github.com>
2024-12-11 20:22:19 +0530

    Add files via upload

---
+++ do_while_loop.java
@@ -0,0 +1,10 @@
+public class do_while_loop {
+    
+    public static void main(String[] args) {
+        int i=0;
+        do{
+            System.out.println("hello world");
+            i++; 
+        }while(i<10);
+    }
+}
