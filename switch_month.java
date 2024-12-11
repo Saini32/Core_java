b495bd3ceb678bfff9b041f870d064be174e3bae
Sneha Mourya <157492674+Saini32@users.noreply.github.com>
2024-12-11 19:15:52 +0530

    Add files via upload

---
+++ switch_month.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+class switch_month {
+    public static void main (String args[])
+    {
+        int months;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number for months=");
+        months=sc.nextInt();
+        switch(months)
+        {
+            case 1:
+            System.out.println("january");
+            break;
+            case 2:
+            System.out.println("february");
+            break;
+            case 3:
+            System.out.println("march");
+            break;
+            case 4:
+            System.out.println("april");
+            break;
+            case 5:
+            System.out.println("may");
+            break;
+            case 6:
+            System.out.println("june");
+            break;
+            case 7:
+            System.out.println("july");
+            break;
+            case 8:
+            System.out.println("august");
+            break;
+            case 9:
+            System.out.println("september");
+            break;
+            case 10:
+            System.out.println("october");
+            break;
+            case 11:
+            System.out.println("november");
+            break;
+            case 12:
+            System.out.println("december");
+            break;
+            default:
+            System.out.println("invalid!!");
+        }
+    }
+
+    
+}
